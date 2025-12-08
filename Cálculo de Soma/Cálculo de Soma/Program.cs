b926042c861396a1f97094696fdfcadbfa3cee94
Jocimar Silva <duvidasclick@gmail.com>
2025-12-08 20:39:34 -0300

    Add files via upload

---
+++ Cálculo de Soma/Cálculo de Soma/Program.cs
@@ -0,0 +1,11 @@
+﻿int n1;
+int n2;
+int soma;
+Console.WriteLine("Digite o primeiro número");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo número");
+n2 = int.Parse(Console.ReadLine());
+soma = n1 + n2;
+Console.WriteLine("A Soma dos números são:");
+Console.WriteLine(soma);
+Console.ReadKey();
