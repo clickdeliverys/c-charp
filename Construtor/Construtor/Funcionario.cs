b28b930899b69e44674b0cbaa9fa61d49dfd1537
Jocimar Silva <duvidasclick@gmail.com>
2026-01-05 20:34:05 -0300

    Add files via upload

---
+++ Construtor/Construtor/Funcionario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtor
+{
+    internal class Funcionario
+    {
+        private int matricula;
+        private string nome;
+        public Funcionario()
+        {
+            matricula = 00;
+            nome = "Nome";
+        }
+        public Funcionario(int matricula, string nome)
+        {
+            this.matricula = matricula;
+            this.nome = nome;
+        }
+        public void setMatricula(int matricula)
+        {
+            this.matricula = matricula;
+        }
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public int getMatricula()
+        {
+            return this.matricula;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void ImprimeValores()
+        {
+            Console.WriteLine("Nome Funcionário: " + nome);
+            Console.WriteLine("Matricula funcionário: " + matricula);
+        }
+        ~Funcionario()
+        {
+            //Método Destrutor
+        }
+    }
+}
---
+++ Construtor/Construtor/Program.cs
@@ -0,0 +1,7 @@
+﻿using Construtor;
+
+Funcionario f1 = new Funcionario();
+f1.Imprimevalores();
+Funcionario f2 = new Funcionario(35, "João Paulo");
+f2.ImprimeValores();
+Console.ReadKey();
