6186cf38ac5a64445ea8fe209843291a7d75edc9
Jocimar Silva <duvidasclick@gmail.com>
2026-01-19 20:47:04 -0300

    Add files via upload

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,20 @@
+﻿int n1;
+Console.WriteLine("Digite um número");
+try
+{
+    n1 = int.Parse(Console.ReadLine());
+    for (int i = 1; i <= 9; i++)
+    {
+        Console.WriteLine(n1 + "x" + i + "=" + (n1 * i));
+    }
+}
+catch (Exception digitou_letra)
+{
+    Console.WriteLine("Você digitou uma letra");
+}
+finally
+{
+    Console.WriteLine("Pressione uma tecla para sair");
+    Console.ReadKey();
+}
+
